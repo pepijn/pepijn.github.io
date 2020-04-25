4d27dc451eb1fa32d6df75261fb5795ad44f2529
Pepijn Looije <p@epij.nl>
2020-04-25 10:42:34 -0300

    Make sure save hooks are local

--- _source/.dir-locals.el
+++ _source/.dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode (eval . (add-hook 'after-save-hook 'pepe/after-org-save))))
+((org-mode (eval . (add-hook 'after-save-hook 'pepe/after-org-save nil t))))
